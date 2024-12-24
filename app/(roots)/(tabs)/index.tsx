87c3d1f20bb6b75058175bc26054377b796e02e9
gyawali9 <roshangyawali9@gmail.com>
2024-12-24 15:40:46 +0545

    feat: assets and fonts setup

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -11,7 +11,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to Restate</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl">
+        Welcome to Restate
+      </Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/profile">Profile</Link>
 
---
+++ image.d.ts
@@ -0,0 +1,20 @@
+declare module "*.png" {
+  const value: unknown;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: unknown;
+  export default value;
+}
+declare module "*.jpeg" {
+  const value: unknown;
+  export default value;
+}
+declare module "*.gif" {
+  const value: unknown;
+  export default value;
+}
+declare module "*.svg" {
+  const value: unknown;
+  export default value;
+}
